3ecd45205cb3a750d8971c3613f20922b0cd1ca4
Carlos-Melo3 <94694859+carlos-aldrim@users.noreply.github.com>
2023-06-11 15:04:20 -0300

    chore: Consertando erros do functions.ts

--- utils/functions.ts
+++ utils/functions.ts
@@ -9,16 +9,17 @@ export const generateRandomString = (length: number) => {
 };
  
 export const generateCodeChallenge = async (codeVerifier: string) => {
-  const base64encode = (string: string) => {
-    return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+  const base64encode = (array: Uint8Array) => {
+    return btoa(String.fromCharCode.apply(null, Array.from(array)))
       .replace(/\+/g, '-')
       .replace(/\//g, '_')
       .replace(/=+$/, '');
-  }
+  }  
 
   const encoder = new TextEncoder();
   const data = encoder.encode(codeVerifier);
   const digest = await window.crypto.subtle.digest('SHA-256', data);
-
-  return base64encode(digest);
+  const digestArray = new Uint8Array(digest);
+  
+  return base64encode(digestArray);
 };
